b93b55b3b92cc6b5ff0d06e45f45e50328b1511e
Tega McKinney <tega.mckinney@flexdrive.com>
2019-08-01 23:30:13 -0400

    Attempt to override regex_replace_chars with same value as default

--- aws/root-dns/ns/main.tf
+++ aws/root-dns/ns/main.tf
@@ -3,14 +3,15 @@ locals {
 }
 
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.5.4"
-  enabled    = "${local.enabled ? "true" : "false"}"
-  namespace  = "${var.namespace}"
-  stage      = "${var.stage}"
-  name       = "${var.name}"
-  delimiter  = "${var.delimiter}"
-  attributes = "${var.attributes}"
-  tags       = "${var.tags}"
+  source              = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.5.4"
+  enabled             = "${local.enabled ? "true" : "false"}"
+  namespace           = "${var.namespace}"
+  stage               = "${var.stage}"
+  name                = "${var.name}"
+  delimiter           = "${var.delimiter}"
+  attributes          = "${var.attributes}"
+  tags                = "${var.tags}"
+  regex_replace_chars = "/[^a-zA-Z0-9-]/"
 }
 
 # Fetch the OrganizationAccountAccessRole ARNs from SSM
