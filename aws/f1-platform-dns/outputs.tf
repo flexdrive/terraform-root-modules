867d09e0baa6cff8f043bf6c29e426ca50ce7870
Tega McKinney <tega.mckinney@flexdrive.com>
2019-09-27 07:36:59 -0400

    Add module for platform dns

---
+++ aws/f1-platform-dns/outputs.tf
@@ -0,0 +1,28 @@
+output "parent_zone_id" {
+  value       = "${join("", null_resource.parent.*.triggers.zone_id)}"
+  description = "ID of the hosted zone to contain this record"
+}
+
+output "parent_zone_name" {
+  value       = "${join("", null_resource.parent.*.triggers.zone_name)}"
+  description = "Name of the hosted zone to contain this record"
+}
+
+output "zone_id" {
+  value       = "${join("", aws_route53_zone.default.*.zone_id)}"
+  description = "Route53 DNS Zone ID"
+}
+
+output "zone_name" {
+  value       = "${replace(join("", aws_route53_zone.default.*.name), "/\\.$$/", "")}"
+  description = "Route53 DNS Zone name"
+}
+
+output "name_servers" {
+  value = "${aws_route53_zone.default.*.name_servers}"
+}
+
+output "fqdn" {
+  value       = "${join("", aws_route53_zone.default.*.name)}"
+  description = "Fully-qualified domain name"
+}
