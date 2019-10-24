325fbee8842d2680d9e2381723c6005e36988213
Hasan Soneji <hasan.soneji@flexdrive.com>
2019-10-24 17:20:08 -0400

    Another long workaround for broken old module

---
+++ aws/jenkins/src/src2/security_group.tf
@@ -0,0 +1,31 @@
+resource "aws_security_group" "datapipeline" {
+  count       = "${var.datapipeline_security_group == "" ? 1 : 0}"
+  tags        = "${module.label.tags}"
+  vpc_id      = "${data.aws_vpc.default.id}"
+  name        = "${module.label.id}"
+  description = "${module.label.id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group_rule" "datapipeline_efs_ingress" {
+  count                    = "${var.modify_security_group == "true" ? 1 : 0}"
+  from_port                = 0
+  protocol                 = "-1"
+  security_group_id        = "${data.aws_efs_mount_target.default.security_groups[0]}"
+  to_port                  = 0
+  type                     = "ingress"
+  source_security_group_id = "${var.datapipeline_security_group == "" ? join("", aws_security_group.datapipeline.*.id) : var.datapipeline_security_group}"
+}
